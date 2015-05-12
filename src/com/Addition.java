54462207c837dfd0fcdf442f9f2ba9604ca263e7
Sharvari <Sharvari@192.168.1.75>
2015-05-12 16:39:06 +0530

    first commit

---
+++ src/com/Addition.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Addition {
+
+	public static void main(String[] args)
+	{
+   int a=20;
+   int b=30;
+  
+   
+   System.out.println("addition is" +(a+b));
+   
+   
+
+	}
+
+}
